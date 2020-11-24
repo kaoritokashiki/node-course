2fc28f07752bcd6e01a6884125fb2e595fa4f322
kaori <kaori@weseek.co.jp>
2020-11-25 00:44:13 +0900

    done 47

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,34 @@
 const path = require('path')
 const express = require('express')
+const { resourceUsage } = require('process')
 
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(publicDirectoryPath)))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help', {
+        title: 'Help',
+        helpText: 'help me'
+    })
+})
 
 
 app.get('/weather', (req, res) => {
