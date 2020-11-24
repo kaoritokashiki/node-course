6ffd9a516ad91dd20774ca5e1c0a8d05a65ebcb2
kaori <kaori@weseek.co.jp>
2020-11-24 13:33:09 +0900

    done 45

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,12 @@
+const path = require('path')
 const express = require('express')
 
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('/', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+app.use(express.static(path.join(publicDirectoryPath)))
 
-app.get('/help', (req, res) => {
-    res.send([{ 
-        neme: 'Andrew',
-        age: 27
-    },
-    { name: 'Kaori'
-
-    }
-])
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
 
 app.get('/weather', (req, res) => {
     res.send({
