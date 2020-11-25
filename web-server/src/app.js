af2b950d0d213c80175ffc80438c289888ad22d5
kaori <kaori@weseek.co.jp>
2020-11-25 23:03:40 +0900

    done 48

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,9 +4,15 @@ const { resourceUsage } = require('process')
 
 
 const app = express()
+
+
+
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
 app.use(express.static(path.join(publicDirectoryPath)))
 
 app.get('', (req, res) => {
