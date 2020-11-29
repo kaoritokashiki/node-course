cfe9a56c133c5b4f825d83dfdc777924d0a0e5fd
kaori <kaori@weseek.co.jp>
2020-11-29 19:47:43 +0900

    done 58

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,7 +7,19 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
     })
 })
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    
+    console.log(location);
+
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error){
             return console.log(data.error);
@@ -17,3 +29,7 @@ fetch('http://localhost:3000/weather?address=boston').then((response) => {
         }
     })
 })
+
+
+})
+
