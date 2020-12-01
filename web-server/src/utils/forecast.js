79db43dee64d4913bd2cde17cc8edf3a8344519c
kaori <kaori@weseek.co.jp>
2020-12-02 01:40:07 +0900

    done 68

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -9,7 +9,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0])
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] + ". It is currently" + body.current.templature + "degress outerHeight. It feels like " +
+                body.current.feelslike + " degrees out. The humidit is " + body.current.humidity + "%."
+            )
         }
     })
 }
