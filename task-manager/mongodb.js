364e3abaa63f5c290418d19fdcb15a0dd415958f
kaori <kaori@weseek.co.jp>
2020-12-12 23:23:03 +0900

    done 78

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,5 +15,44 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
+    // db.collection('users').findOne({ _id: new ObjectID('5fd4c2c0aa4882c70b373e60') }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch');
+    //     }
+
+    //     console.log(new ObjectID('5fd4c2c0aa4882c70b373e60'));
+    //     console.log(new ObjectID('5fd4c2c0aa4882c70b373e60'));
+
+    //     console.log(user);
+    // })
+
+
+    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
+    //     console.log(users);
+    // })
+
+    // db.collection('users').find({ age: 27 }).count((error, count) => {
+    //     console.log(count);
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID('5fd4bf120f2ddac3c3821473') }, (error, task) => {
+        if(error){
+            return console.log('Unable to fetch');
+        }
+
+        return console.log(task);
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        if (error) {
+            return console.log(error);
+        }
+
+
+        return console.log(tasks);
+    } )
+
+
+
 });
 
