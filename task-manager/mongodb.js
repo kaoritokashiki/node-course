1f7468dd95dc19b914240f40d518077e8dbcfe29
kaori <kaori@weseek.co.jp>
2020-12-12 22:24:13 +0900

    done 77

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,8 +1,12 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error){
@@ -10,50 +14,6 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
-
-    // db.collection('users').insertOne({
-    //     name: 'Andrew',
-    //     age: 27
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user');
-    //     }
-
-    //     console.log(result.ops);
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name:'Jen',
-    //         age: 28
-    //     }, {
-    //         name: 'Gunther',
-    //         age: 27
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents!');
-    //     }
-    //     console.log(result.ops);
-    // })
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'Clean the house',
-            completed: true
-        }, {
-            description: 'Renew inspection',
-            completed: false
-        }, {
-            description: 'Pot plants',
-            completed: false
-        }
-    ], (error, result) => {
-        if(error){
-            return console.log('Unable to insert documents');
-        }
-        console.log(result.ops);
-    })
 
 });
 
