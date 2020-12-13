19217f469353777ba9ea9731249ef2377552e513
kaori <kaori@weseek.co.jp>
2020-12-13 20:59:00 +0900

    done 95

---
+++ task-manager/playground/promise-chaining-2.js
@@ -0,0 +1,11 @@
+require('../src/db/mongoose');
+const Task = require('../src/models/task');
+
+Task.findByIdAndDelete("5fd5834d2b75e0e6aa39eb88").then((task) => {
+    console.log(task);
+    return Task.countDocuments({ completed: false})
+}).then((result) => {
+    console.log(result);
+}).catch((e) => {
+    console.log(e);
+})
