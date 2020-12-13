d8ddc0c8276c5d7f48e4250ccc121da4a76f478d
kaori <kaori@weseek.co.jp>
2020-12-13 10:33:47 +0900

    done 80

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -15,44 +15,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({ _id: new ObjectID('5fd4c2c0aa4882c70b373e60') }, (error, user) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch');
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID('5fd4aa5cc3c69cc19a97a022')
+    // }, {
+    //     $inc: {
+    //         age: -1
     //     }
-
-    //     console.log(new ObjectID('5fd4c2c0aa4882c70b373e60'));
-    //     console.log(new ObjectID('5fd4c2c0aa4882c70b373e60'));
-
-    //     console.log(user);
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
     // })
 
-
-    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
-    //     console.log(users);
-    // })
-
-    // db.collection('users').find({ age: 27 }).count((error, count) => {
-    //     console.log(count);
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectID('5fd4bf120f2ddac3c3821473') }, (error, task) => {
-        if(error){
-            return console.log('Unable to fetch');
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: { 
+            completed: true
         }
-
-        return console.log(task);
+    }).then((result) => {
+        console.log(result.modifiedCount);
+    }).catch((error) => {
+        console.log(error);
     })
-
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        if (error) {
-            return console.log(error);
-        }
-
-
-        return console.log(tasks);
-    } )
-
-
-
-});
-
+})
