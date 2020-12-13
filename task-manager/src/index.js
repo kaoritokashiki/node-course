4fc5a54b048d38651c7746880c3da63e13e09112
kaori <kaori@weseek.co.jp>
2020-12-13 16:51:36 +0900

    done 90

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,7 +12,18 @@ app.use(express.json())
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        console.log(req.body);
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
