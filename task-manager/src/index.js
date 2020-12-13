f911be96b3c48473c209dfc2dfcdaae7fc900de3
kaori <kaori@weseek.co.jp>
2020-12-14 02:38:00 +0900

    done 98

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -45,6 +45,27 @@ app.get('/users', async (req, res) => {
     // })
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const inValidOperation = updates.every((update) => allowedUpdates.includes((update)))
+
+    if (!inValidOperation){
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
@@ -52,7 +73,7 @@ app.get('/users/:id', async (req, res) => {
         const user = await User.findByIdAndDelete({_id})
 
         if(!user){
-            res.status(404).send()
+            return res.status(404).send()
         }
         res.send(user)
     } catch (e) {
